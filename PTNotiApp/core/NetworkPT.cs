ac8a79c050d6b924dc23d17a18786c02585a3b37
gsb-b60 <dihieu2407@gmail.com>
2025-12-04 23:14:55 +0700

    feat connect to server
    Signed-off-by: gsb-b60 <dihieu2407@gmail.com>

---
+++ PTNotiApp/core/NetworkPT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTNotiApp.core
+{
+    class NetworkPT
+    {
+        private IPAddress _serverIP = IPAddress.Parse("127.0.0.1");
+        private int _serverPort = 5000;
+        private TcpClient _client;
+
+        public StreamWriter _writer;
+        public StreamReader _reader;
+        public async Task ConnectToServer()
+        {
+            try
+            {
+                _client = new TcpClient();
+                await _client.ConnectAsync(_serverIP, _serverPort);
+                var stream=_client.GetStream();
+                _writer=new StreamWriter(stream) { AutoFlush=true};
+                _reader=new StreamReader(stream);
+
+
+            }catch(Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+    }
+}
