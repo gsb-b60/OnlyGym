5f25740eee3cdc154c17334e3d1f39ee0bfeda5a
gsb-b60 <dihieu2407@gmail.com>
2025-12-05 22:04:57 +0700

    feat tcp server
    Signed-off-by: gsb-b60 <dihieu2407@gmail.com>

---
+++ GymManagerment_MVP/NetworkCore/NetworkServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagerment_MVP.NetworkCore
+{
+    public class NetworkServer
+    {
+        private TcpListener listener;
+        private readonly Idatabasehandler db;
+        private Action<String> log;
+    }
+}
